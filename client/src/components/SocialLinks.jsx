1ae280d09ca2187516155299bc75c9325911844c
BInBilibili <abc3556001@163.com>
2026-09-23 12:45:04 +0800

    chore: vendor Font Awesome + Bangumi icon locally (drop external CDN)

--- client/src/components/SocialLinks.jsx
+++ client/src/components/SocialLinks.jsx
@@ -45,7 +45,7 @@ function SocialLinks({ onSettingsClick, onHelpClick, onFeedbackClick, showFeedba
       )}
 
       <a href="https://bangumi.tv/user/725027" target="_blank" rel="noopener noreferrer" className="social-link">
-        <img src="https://avatars.githubusercontent.com/u/7521082?s=200&v=4" alt="Bangumi" className="bangumi-icon" />
+        <img src={`${import.meta.env.BASE_URL}assets/bangumi-icon.png`} alt="Bangumi" className="bangumi-icon" />
       </a>
       <a href="https://github.com/kennylimz/anime-character-guessr" target="_blank" rel="noopener noreferrer" className="social-link">
         <i className="fab fa-github"></i>
